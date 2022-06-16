6fac596332bbd1e84050e84d94c7f11eb9dc99f4
Kris Hernandez <krismac@kriss-air.mynetworksettings.com>
2022-06-16 08:52:03 -0400

    working views

--- counter_proj/word_app/models.py
+++ counter_proj/word_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    search_word = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.search_word
+
+class Document(models.Model): 
+    title = models.CharField(max_length=300)
+    year = models.IntegerField(max_length=4)
+    author = models.CharField(max_length=300)
+    words = models.ManyToManyField(Word, related_name='documents')
+    file = models.FileField()
+    def __str__(self):
+        return self.title
+
+
+class WordCount(models.Model):
+    transcript = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='word_counts')
+    word = models.ForeignKey(Word,on_delete=models.CASCADE, related_name='word_counts')
+    count = models.IntegerField(default=1)
+    
+    pass
+
--- counter_proj/word_app/views.py
+++ counter_proj/word_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from . models import Word, Document, WordCount 
 
 # Create your views here.
 def index(request): 
+    pass
+
+def word_count(request): 
+    form = request.POST
+    author_name = form.get('author')
+    title = form.get('title')
+    year = form.get('year')
+
     pass
